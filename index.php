0a25504fcd71756bde3367a6421b6416ecb806a6
suaveguy <tshepo@openlearn.co.za>
2019-09-08 13:20:15 -0700

    modified style.css ,modified index.php and added an agripic.png

--- index.php
+++ index.php
@@ -13,17 +13,20 @@
 </head>
 <body>
 <div class="header">
-    <a href="#default" class="logo">HOME & CO</a>
-  <div class="header-right">
+    <a href="#default" class="logo">DE-BALOS</a>
+  <div class="header-center">
     <a href="home">HOME</a>
+    <a href="#services">SERVICES</a>
+    <a href="#community">COMMUNITY</a>
+    <a href="#seeds">SEEDS</a>
     <a href="#contact">CONTACT</a>
-    <a href="#about">ABOUT-US</a>
+    
   </div>
 </div>
 <div class="bg-image"></div>
 
 <div class="bg-text">
-  <h1 style="font-size:50px">WELCOME TO MY HOMEPAGE</h1>
+  <h1 style="font-size:50px">GOING GREEN IS EASY</h1>
 </div>
 
 <!--====== Portfolio Gallery Grid======-->
@@ -51,86 +54,40 @@
   </div>
   </div>
 
-<!--=========About us=========-->
-  <div class="arrow_box">
-  
-  <h1 class="about-par" style="color:#FFFFFF;">WE JUST NEED A FEW MORE DETAILS.... </h1>
 
-<div class="section1">
-  <div class="par1">
-    <p>Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide). Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide). Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).</p>
-  </div>
-  <div class="par2"> 
-    <p>Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).</p>
-  </div>
-  <div class="par3">
-    <img src="pic1_1.jpg" alt="Nature" style="width:410px;height:580px;">
-  </div>
-</div><br><br><br>
-  <div class="par4" style="width:100%;overflow-x:hidden;">
-  <p> instead of floating next to each other, when the screen is less than 600px wide.
-  Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide).Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating next to each other, when the screen is less than 600px wide). Some text..Resize the browser window to see the responsive effect (the columns will stack on top of each other instead of floating </p>
-</div>
-  </div>
-
-<!---====image section ======---->
-
-<div class="image-sec">
-    <img src="pic1.jpg" alt="Mountains" style="width:100%;height:800px;">
-</div>
 
 <!---=====contact section======--->
 <div class="container">
-  <div class="row2">
-    <div class="column2">
-   
+    <h1 class="H2">CONTACT</h1>
+    <hr>
     <h4 class="h4">Fill out the form our team will be in touch with you.<br>Thank you for your interest.</h4>
  
       <form action="/action_page.php">
-        <label for="yname">Your Name</label>
+       
         <input type="text" id="yname" name="yourname" placeholder="Your name..">
-        <label for="yemail">Your Email</label>
+      
         <input type="text" id="yemail" name="youremail" placeholder="Your email..">
-        <label for="ynumber">Your Number</label>
+       
         <input type="text" id="ynumber" name="yournumber" placeholder="Your number..">
-        <label for="subject">Subject</label>
-        <input type="text" id="subject" name="subject" placeholder="Subject..">
-        <label for="message">Message</label>
-        <textarea id="message" name="message" placeholder="Write something.." style="height:170px"></textarea>
+       
         <input type="send" value="SEND">
       </form>
-     
-     
-    </div>
-    <div class="column2">
-    
-<div class="column1">
-  <div class="row1">
-    <h2>ABOUT US</h2>
-    <p class="row1-par">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et. </p>
-  </div>
-  
-  <div class="row-soc">
-    <h2>SOCIAL MEDIA</h2>
-    
-<div class="icon-bar">
+         </div> 
+      <div class="footer">
+       <div class="icon-bar">
 	<a href="#"><i class="fa fa-facebook"></i></a>
 	<a href="#"><i class="fa fa-twitter"></i></a>
 	<a href="#"><i class="fa fa-instagram"></i></a>
 	<a href="#"><i class="fa fa-linkedin"></i></a>
 	<a href="#"><i class="fa fa-youtube"></i></a>
-  </div>
-    
-  <div class="row-touch">
-    <h2 class="h2">GET IN TOUCH</h2>
-    <p>Address:73 juta street braamfontein</p>
-    <p>Email:info@buildmysite.co.za</p>
-    <p>Contact:+27 81 234 5757</p>
-  </div>
-</div>
-    </div>
-    </div>
-  </div>
-    </div>
+        </div>
+          <p><span>&copy;Abe Designs 2019</span></p>
+          <div class="contact">
+	       <i class="fa fa-phone"><p>+27735231123</p></i>
+              
+			<i class="fa fa-envelope"><p><a href="mailto:support@eduonix.com">abeltshepo16@gmail.com</a></p></i>
+			
+		</div>
+      </div>
 </body>
 </html>
