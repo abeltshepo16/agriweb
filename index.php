7a42b981dadb07a63a7d1cd5e8746dd3c600e443
suaveguy <tshepo@openlearn.co.za>
2019-09-19 12:15:09 -0700

    modified style.css and index.php

--- index.php
+++ index.php
@@ -26,53 +26,128 @@
 <div class="bg-image"></div>
 
 <div class="bg-text">
-  <h1 style="font-size:50px">GOING GREEN IS EASY</h1>
+  <h1>GOING GREEN IS EASY</h1>
+  <p style="font-size:20px">Making the world greener is easy one <br>community at time.</p>
+  <button class="greenbtn">ORDER SEEDS</button>
 </div>
 
 <!--====== Portfolio Gallery Grid======-->
 <div class="row">
   <div class="column">
     <div class="content">
-      <img src="pic1.jpg" alt="Mountains" style="width:100%;height:300px;">
+      <img src="pic1.jpg" alt="Mountains" style="width:100%;height:300px;border-radius: 50%;">
       <h3>SERVICE</h3>
       
     </div>
   </div>
   <div class="column">
     <div class="content">
-    <img src="pic2.jpg" alt="Lights" style="width:100%;height:300px;">
+    <img src="pic2.jpg" alt="Lights" style="width:100%;height:300px;border-radius: 50%;">
       <h3>STUFF</h3>
       
     </div>
   </div>
   <div class="column">
     <div class="content">
-    <img src="pic3.jpg" alt="Nature" style="width:100%;height:300px;">
+    <img src="pic3.jpg" alt="Nature" style="width:100%;height:300px;border-radius: 50%; ">
       <h3>ABOUT US</h3>
 
     </div>
   </div>
   </div>
+<!--======About Section=====----->
 
+<div class="row-abt" style="background:lightgreen">
+  <div class="col-abt">
+      <h1 style="text-align:left">ABOUT US</h1><hr class="hr1">
+    <div class="contents1">
+      <h3>Lifting Community Needs</h3>
+      <p class="par1">We started recognizing a need in our community .A local<br>pop-up market went out business during the recession and<br> left 2.5 acress of abandoned land .We envisioned a community<br> Balos garden where people could meet and share their love for<br> plants,gardening and helping the environment  
+      </p>
+    </div>
+       <div class="contents1">
+      <h3>Uniting Communities</h3>
+      <p class="par2">Since 2001 ,our mission has been to bring the community <br>together .provide education on sustainable gardening practices.<br>and help  the environment  thrive.we're done this by <br>developing a space where people can have their own plots.<br>where others can work on shared plots and raising funds to<br> build greenhouse.We offer weekly classes on sustainability <br>practices composting and more so that the community can<br> take their skills to their home gardens.</p>
+    </div>
+  </div>
+  <div class="pic-abt">
+    <img src="agri3.png" alt="agri3" style="width:50%;height:400px;">
+    </div>
+  </div>
 
+<!--======new section======--->
+<div class="row-dif" style="background:creamwhite">
+  <div class="col-dif">
+      <h1 style="text-align:left">Make a Difference</h1><hr class="hr2">
+    <div class="contents2">
+      <h3>No For Profit</h3>
+      <p class="par3">
+          Our organization is always looking for new people that has a <br>
+          green thumb or is looking to develop one As a non-profit<br>
+          organization .we rely on our volunteers to help make our<br>
+          spaces greener and our lives better!
+      </p>
+    </div>
+       <div class="contents2">
+      <h3>Support Your Community</h3>
+      <p class="par4">
+          We support communities that are looking to grow or develop<br>
+          green spaces by selling materials and products from our<br>
+          gardens .if you're interested in joining us or donating to our<br>
+          programs.feel free to contact us!
+      </p>
+    </div>
+       <div class="contents2">
+      <h3>Want to Help?</h3>
+      <p class="par5">
+          We're always looking for volunteers for all kinds of roles in our<br>
+          group,like program leaders.gardening help.fresh vegetables<br>
+          and plant salespeople and more Anyone of any age can help<br>
+      </p>
+    </div>
+  </div>
+  <div class="pic-dif">
+    <img src="agri4.png" alt="agri4" style="width:50%;height:400px;">
+    </div>
+  </div>
 
 <!---=====contact section======--->
 <div class="container">
-    <h1 class="H2">CONTACT</h1>
-    <hr>
-    <h4 class="h4">Fill out the form our team will be in touch with you.<br>Thank you for your interest.</h4>
- 
-      <form action="/action_page.php">
-       
-        <input type="text" id="yname" name="yourname" placeholder="Your name..">
-      
-        <input type="text" id="yemail" name="youremail" placeholder="Your email..">
-       
-        <input type="text" id="ynumber" name="yournumber" placeholder="Your number..">
-       
+  <div class="row2">
+    <div class="columna">
+      <form action="/action_page.php">      
+        <input type="text" id="yname" name="yourname" placeholder="Your name..">       
+        <input type="text" id="yemail" name="youremail" placeholder="Your email..">     
+        <input type="text" id="ynumber" name="yournumber" placeholder="Your number..">       
+        <input type="text" id="subject" name="subject" placeholder="Subject..">
+        <textarea id="message" name="message" placeholder="Write something.." style="height:135px"></textarea>
         <input type="send" value="SEND">
       </form>
-         </div> 
+    </div>
+    
+<div class="columnb">
+  <div class="row1">
+    <h2 class="h2-con">Volunteer Opportunities</h2>
+    <p class="row1-par">
+        You would be helping us dig plant,weed and grow the garden.Volunteers are welcome<br> during the week and are recommended to bring water,sunscreen,<br>comfortable clothing and close-toed shoes. 
+    </p>
+  </div>
+  
+  <div class="row-par2">
+    <h2>Light Construction</h2>
+    Our community often share or have their own plots.We need assistance with <br> building and maintaining the construction of plots and other things in our <br> garden.
+    </div>
+    
+  <div class="row-par3">
+    <h2 class="h2">Classes</h2>
+    <p>If you would like to teach a class feel free to send us your ideas.</p>
+  </div>
+</div>
+    </div>
+    </div>
+  </div>
+    
+
       <div class="footer">
        <div class="icon-bar">
 	<a href="#"><i class="fa fa-facebook"></i></a>
@@ -80,14 +155,11 @@
 	<a href="#"><i class="fa fa-instagram"></i></a>
 	<a href="#"><i class="fa fa-linkedin"></i></a>
 	<a href="#"><i class="fa fa-youtube"></i></a>
-        </div>
-          <p><span>&copy;Abe Designs 2019</span></p>
-          <div class="contact">
-	       <i class="fa fa-phone"><p>+27735231123</p></i>
-              
-			<i class="fa fa-envelope"><p><a href="mailto:support@eduonix.com">abeltshepo16@gmail.com</a></p></i>
-			
-		</div>
+       </div> 
+         
+              <h5 class="H5">Copy rights &COPY; 2019 DE-Balos Enterprice All Rights Reserved</h5>
+          <h6 class="h6">Powered By Abe Designs</h6>
+         
       </div>
 </body>
 </html>
